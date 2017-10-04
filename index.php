50c4dbeba8dd2004972b05e30a9651aa17c710e8
Akoni <hakmorrison@csumb.edu>
2017-10-04 00:06:58 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Homework 4</title>
+    </head>
+    <body>
+        <h1>HTML Forms / PHP</h1>
+        <h2>Registration Form</h2>
+        
+
+        
+        <form>
+            <input type="text" name="firstname" placeholder="First Name">
+            <input type="text" name="lastname" placeholder="Last Name">
+            <input type="text" name="username" placeholder="Username">
+            <input type="password" name="password" placeholder="Password">
+            <input type="date" name="bday">
+            <input type="email" name="email" placeholder="E-mail">
+            <input type="reset">
+            <input type="submit" value="Submit">
+        </form>
+        
+        <?php
+        
+        ?>
+        
+    </body>
+</html>
